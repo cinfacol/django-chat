41e4aace0f995ff8420f010dd74b76248bdf5103
jorge <cinfacol@gmail.com>
2023-04-23 00:32:04 -0500

    puesta en marcha backend

---
+++ config/asgi.py
@@ -0,0 +1,19 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+
+django.setup()
+
+from app.chat.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from apps.chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
